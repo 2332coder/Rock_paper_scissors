3b05394aa7516cccfd676c2673cf25cefab6f7af
Sambu <sambubsidharth@gmail.com>
2025-02-09 17:13:51 +0530

    finished the project

--- script1.js
+++ script1.js
@@ -1,27 +1,39 @@
 let humanScore=0,computerScore=0;                    //initialising the code
 
-                                                    //selectors for showing the score card
+let roundWinner=document.querySelector('#round-winner');
+
+                                                //selectors for showing the score card
 let human_scorecard=document.querySelector('.human-scorecard');
 let computer_scorecard=document.querySelector('.computer-scorecard');
 
 
 let human_name=document.querySelector('.human-name');
 let getName=()=>{
-    const name=prompt('Enter your god name');
+    const name=prompt('Enter your  name');
     human_name.innerText=name;
+    if(name==""){
+        human_name.innerText="human";
+    }    
+    
 }
-//getName();
+getName();
 
 
-function getComputerChoice(){                       //find the choice of computer
+                        //find the choice of computer
+let computerChoiceId=document.querySelector('#computer-choice-id');
+function getComputerChoice(){                                               
     let x=Math.floor(3*Math.random());
-    if(x==0){                                   
+    if(x==0){         
+        computerChoiceId.src="images/stone.svg";                       
         return "rock";
     }
     else if(x==1){
+
+        computerChoiceId.src="images/paper.svg";       
         return "paper";
     }
     else{
+        computerChoiceId.src="images/scissors.svg";
         return "scissors";
     }
 }
@@ -31,33 +43,63 @@ function playRound(humanSelection, computerSelection) {      //function for play
     if(computerSelection=="rock"){
         if(humanSelection=="paper"){
             humanScore++;
+            roundWinner.innerText=`you won`;
         }
         else if(humanSelection=="scissors"){
             computerScore++;
+            roundWinner.innerText=`computer won this round`;
         }
     }
     
     if(computerSelection=="paper"){
         if(humanSelection=="scissors"){
             humanScore++;
+            roundWinner.innerText=`you won this round`;
         }
         else if(humanSelection=="rock"){
             computerScore++;
+            roundWinner.innerText=`computer won this round`;
         }
     }
 
     if(computerSelection=="scissors"){
         if(humanSelection=="rock"){
             humanScore++;
+            roundWinner.innerText=`you won this round`;
         }
         else if(humanSelection=="paper"){
             computerScore++;
+            roundWinner.innerText=`computer won this round`;
         }
     }
     console.log(`human:${humanSelection}   computer:${computerSelection} \n score: ${humanScore} : ${computerScore}`);
     human_scorecard.innerHTML=humanScore;
     computer_scorecard.innerHTML=computerScore;
+
+    if(humanScore===5){
+        setTimeout(() => {
+            alert(`You won the game! Up for another?`);
+            resetGame();
+        }, 100);
+
+    }
+    else if(computerScore===5){
+        setTimeout(() => {
+            alert(`Computer won the game! Revenge?`);
+            resetGame();
+        }, 100);
+    }
 }
+
+function resetGame() {
+    humanChoiceId.src = "";
+    computerChoiceId.src = "";
+    humanScore = 0;
+    computerScore = 0;
+    human_scorecard.innerHTML = humanScore;
+    computer_scorecard.innerHTML = computerScore;
+    roundWinner.innerText="";
+}    
 
 
 
--- script2.js
+++ script2.js
@@ -1,11 +1,15 @@
-                                                
-                                                
+
+let humanChoiceId=document.querySelector("#human-choice-id");                        //used to change the pic upon human selection                      
+                                       
+
                                                 /* event for rock */    
 
 const rock=document.querySelector('.choice-btn-rock');
 rock.addEventListener("click",()=>{
     let humanSelection="rock"                               
     let computerSelection=getComputerChoice();
+
+    humanChoiceId.src="images/stone.svg";                          //replaces the previous image with the current choice
     playRound(humanSelection,computerSelection);
 });
 
@@ -15,6 +19,7 @@ const paper=document.querySelector('.choice-btn-paper');
 paper.addEventListener("click",()=>{
     let humanSelection="paper"                               
     let computerSelection=getComputerChoice();
+    humanChoiceId.src="images/paper.svg";
     playRound(humanSelection,computerSelection);
 });
 
@@ -23,6 +28,7 @@ const scissors=document.querySelector('.choice-btn-scissors');
 scissors.addEventListener("click",()=>{
     let humanSelection="scissors"                               
     let computerSelection=getComputerChoice();
+    humanChoiceId.src="images/scissors.svg";
     playRound(humanSelection,computerSelection);
 });
 
